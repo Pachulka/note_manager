9791d39d817f95af959a9779bdf52b71d0fb0cad
Pachulka <kapatich07@gmail.com>
2025-01-12 18:07:33 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,6 @@
+created_date = input("Введите дату начала задачи:")
+issue_date = input("Введите окончания задачи:")
+temp_issue_date = created_date[0:5]
+temp_created_date = issue_date[0:5]
+print(f"Дата начала задачи:{temp_issue_date}")
+print(f"Дата озончаня задачи:{temp_created_date}")
