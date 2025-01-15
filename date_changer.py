c815923b54d5094f45878abf745845c87f1c3879
Pachulka <kapatich07@gmail.com>
2025-01-15 18:04:49 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -2,5 +2,6 @@ created_date = input('Введите дату начала заметки:')
 issue_date = input('Введите дату окончания заметки:')
 temp_issue_date = created_date[0:5]
 temp_created_date = issue_date[0:5]
+
 print(f'Дата начала заметки: {temp_issue_date}')
 print(f'Дата озончаня заметки: {temp_created_date}')
